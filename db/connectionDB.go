52ce074d214b52abecb25b79eb591d409d59502a
Jose Iriarte <joseiriarte2102@gmail.com>
2022-03-24 19:42:58 -0400

    Config environment variables

--- db/connectionDB.go
+++ db/connectionDB.go
@@ -3,17 +3,24 @@ package db
 import (
 	"context"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 /*Mongoose => objeto de conexion a la base de datos*/
 var Mongoose = ConnectionDB()
-var clientOptions = options.Client().ApplyURI("mongodb+srv://sysadmin:sysadmin365@clusterdaemon.zqpwr.mongodb.net/db_twittor?retryWrites=true&w=majority")
 
 /*ConnectionDB => conectar a la Base de datos*/
 func ConnectionDB() *mongo.Client {
+	loadEnv()
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	clientOptions := options.Client().ApplyURI("mongodb+srv://" + user + ":" + pass + "@" + host + ".zqpwr.mongodb.net/db_twittor?retryWrites=true&w=majority")
+
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -37,4 +44,11 @@ func CheckConnection() int {
 		return 0
 	}
 	return 1
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/rs/cors v1.8.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	go.mongodb.org/mongo-driver v1.8.4 // indirect
